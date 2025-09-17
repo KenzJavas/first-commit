84127961f2bbe0bfc2738e59f913d5f3ca8e45c1
KenzJavas <kenziejavasniscala756@gmail.com>
2025-09-17 13:51:14 +0700

    belajar mutable dan immutable

---
+++ 2._const_final.dart
@@ -0,0 +1,43 @@
+//  font fira code
+// data yg bisa di ubah itu = mutable
+// data yg tidak bisa diubah = immutable
+// tipe data immutable itu di tandai dengan
+// const = compile time constant
+// final = run time constant
+
+
+const phi = 3.14;//ini adalah immutable
+
+  const namaAwal = "Kenzie"; //imutable
+
+main() {
+  var radius = 7;
+  radius = 10; //ini adalah mutable
+
+  print("luas lingkaran adalah ${luaslingkaran(radius)}");
+
+  // 3,14 * 10 * 10
+
+
+  final
+
+  print(("namaku ${namalengkap}"))
+
+
+}
+
+num namalengkap(){
+  return namaAwal + namaTengah;
+}
+
+num luaslingkaran(num radius){
+  return phi * radius * radius;
+}
+
+
+  // 1. immutable / tidak bisa di ubah 
+  // 1.const dan final
+  // phi itu nilai tetap 3,14 dan 22/7
+
+  // buatkan 2 variable muttable dan immutable
+  // ketentuanya 2 variable mutable berupa string 
